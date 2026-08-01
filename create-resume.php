e900507c7669b3b7ded28755cd132b2c3b807ee2
HanumGoo <sheehanandya001@gmail.com>
2026-08-01 15:13:42 +0700

    updates

--- create-resume.php
+++ create-resume.php
@@ -114,7 +114,7 @@ if (isset($_POST['save'])) {
                     <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
                         <div>
                             <h3 class="fw-bold mb-3">
-                                <?php echo isset($_GET['edit']) ? "Edit Contact" : "Create New Contact" ?>
+                                <?php echo isset($_GET['edit']) ? "Edit Resume" : "Create New Resume" ?>
                             </h3>
                         </div>
                     </div>
